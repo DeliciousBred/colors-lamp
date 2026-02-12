9d9f332489c020b2a983043fb31738f9437ee26a
Andrew <andrew1007r@gmail.com>
2026-02-12 17:21:47 -0500

    create add color api endpoint

---
+++ public/LAMPAPI/AddColor.php
@@ -0,0 +1,44 @@
+<?php
+	$inData = getRequestInfo();
+	
+	$color = $inData["color"];
+	$userId = $inData["userId"];
+
+	$host = ""; // Insert host here
+	$user = ""; // Insert user here
+	$password = ""; // Insert password
+	$database = ""; // Insert database here
+
+	$conn = new mysqli($host, $user, $password, $databse);
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
+		$stmt->bind_param("ss", $userId, $color);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
