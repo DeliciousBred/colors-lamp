cc6a18be5f0c9467ae7bb4634d69e8e96ff82e96
DeliciousBread <andrew1007r@gmail.com>
2026-04-22 16:56:50 -0400

    remove console debugging

--- tests/cookie.test.js
+++ tests/cookie.test.js
@@ -4,8 +4,6 @@ describe("saveCookie function", () => {
   it("sets document.cookie to the correct new cookie", () => {
     saveCookie("Test", "User", 1);
 
-    console.log(document.cookie);
-
     expect(document.cookie).toBe("firstName=Test,lastName=User,userId=1");
-  })
+  });
 })
