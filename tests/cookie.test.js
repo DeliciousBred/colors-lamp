3603ca72de98734c3fd33d2139efd0f164e804f8
DeliciousBread <andrew1007r@gmail.com>
2026-04-22 10:39:46 -0400

    create unit testing for saveCookie function

---
+++ tests/cookie.test.js
@@ -0,0 +1,11 @@
+import {saveCookie} from "../public/js/code.js";
+
+describe("saveCookie function", () => {
+  it("sets document.cookie to the currect new cookie", () => {
+    saveCookie("Test", "User", 1);
+
+    console.log(document.cookie);
+
+    expect(document.cookie).toBe("firstName=Test,lastName=User,userId=1");
+  })
+})
