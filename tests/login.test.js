57db7a407ad52b5d154aa302a98bfef9626fcf67
DeliciousBread <andrew1007r@gmail.com>
2026-04-22 15:51:48 -0400

    switch integration testing to use direct http fetching

--- tests/login.test.js
+++ tests/login.test.js
@@ -2,8 +2,22 @@ import { doLogin } from "/public/js/code.js";
 
 describe("POST /LAMPAPI/Login.php", () => {
   it("should return success", async () => {
-    const userId = doLogin("http://localhost:8000/Login.php", "Test", "test");
 
-    console.log(userId);
+    const response = await fetch("http://localhost:8000/Login.php", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        login: "Test",
+        password: "test"
+      })
+    });
+
+    console.log(response.body);
+
+    //const userId = doLogin("http://localhost:8000/Login.php", "Test", "test");
+
+    //console.log(userId);
   });
 });
